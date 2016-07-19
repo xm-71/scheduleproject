d20f5c51e4238dec8e65ee3afbcfb4f924bf17d9
xm-71 <alexcannon2@live.com>
2016-07-19 10:39:23 -0400

    app fully synced to firebase with days repeating

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // create our angular module and inject firebase
 angular.module('scheduleApp', ['firebase'])
 
@@ -6,13 +5,14 @@ angular.module('scheduleApp', ['firebase'])
 .controller('mainController', function($scope, $firebase) {
 
   // connect to firebase
-  var ref = new Firebase("https://scheduler-c3dfc.firebaseio.com/days");  
+  var ref = new Firebase("https://burning-torch-4263.firebaseio.com/days");
   var fb = $firebase(ref);
 
   // sync as object
-  var syncObject = fb.$asObject();
+    var syncObject = fb.$asObject();
 
-  // three way data binding
-  syncObject.$bindTo($scope, 'days');
+    // three way data binding
+    syncObject.$bindTo($scope, 'days');
+
 
 });
