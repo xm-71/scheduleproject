da818d346d6fff29496ed15cb3be99aa4560cf69
xm-71 <alexcannon2@live.com>
2016-07-19 10:02:58 -0400

    Synced to firebase and loading time slot

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
+
+
 // create our angular module and inject firebase
 angular.module('scheduleApp', ['firebase'])
 
 // create our main controller and get access to firebase
 .controller('mainController', function($scope, $firebase) {
 
-  // our application code will go here
+  // connect to firebase
+  var ref = new Firebase("https://scheduler-c3dfc.firebaseio.com/");
+  var fb = $firebase(ref);
+
+  // function to set the default data
+  $scope.reset = function() {
+
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });
+
+  };
 
 });
