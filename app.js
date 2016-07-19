37635afa97c6b3bc2d5b7e06e306a964cb60392f
xm-71 <alexcannon2@live.com>
2016-07-19 10:24:53 -0400

    data syncing to firebase

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 // create our angular module and inject firebase
 angular.module('scheduleApp', ['firebase'])
 
@@ -7,41 +6,13 @@ angular.module('scheduleApp', ['firebase'])
 .controller('mainController', function($scope, $firebase) {
 
   // connect to firebase
-  var ref = new Firebase("https://scheduler-c3dfc.firebaseio.com/");
+  var ref = new Firebase("https://scheduler-c3dfc.firebaseio.com/days");  
   var fb = $firebase(ref);
 
-  // function to set the default data
-  $scope.reset = function() {
+  // sync as object
+  var syncObject = fb.$asObject();
 
-    fb.$set({
-      monday: {
-        name: 'Monday',
-        slots: {
-          0900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          }
-        }
-      },
-      tuesday: {
-        name: 'Tuesday',
-        slots: {
-          0900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          }
-        }
-      }
-    });
-
-  };
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
 
 });
