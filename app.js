059e0b9d3ee3d54cb194bb9f8f6722d5dbfc0ebf
xm-71 <alexcannon2@live.com>
2016-07-19 09:55:04 -0400

    Basic page layout set up with styling

---
+++ app.js
@@ -0,0 +1,9 @@
+// create our angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+
+  // our application code will go here
+
+});
